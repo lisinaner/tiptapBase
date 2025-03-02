ab4d92f769db75351f5e5486103b2e24c07480d1
lsn <1064404481@qq.com>
2025-03-02 09:16:06 +0800

    1

--- packages/extension-paragraph/src/paragraph.ts
+++ packages/extension-paragraph/src/paragraph.ts
@@ -47,6 +47,7 @@ export const Paragraph = Node.create<ParagraphOptions>({
   },
 
   renderHTML({ HTMLAttributes }) {
+    console.trace()
     return ['p', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
   },
 
