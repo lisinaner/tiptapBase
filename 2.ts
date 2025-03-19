8fa44f25eaaca160ab817e8840d5a49880885d90
lsn <1064404481@qq.com>
2025-03-20 05:57:19 +0800

    1

--- 2.ts
+++ 2.ts
@@ -4,6 +4,7 @@ import {html_beautify} from "js-beautify"
 import { Editor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
 import UniqueID from "@tiptap/extension-tiptap-unique-id"
+import { getDebugJSON } from "@tiptap/core";
 document.body.innerHTML = `<div id="element"></div>`;
 // console.log(UniqueID)
 const element=document.querySelector('#element');
@@ -14,14 +15,29 @@ if(element){
         extensions: [StarterKit,  UniqueID.configure({
             types: ['heading', 'paragraph'],
           })],
-        content: '<p> 123</p>',
+        content: `
+        
+         <ul>
+          <li>This is a bullet list.</li>
+          <li>And it has three list items.</li>
+          <li>Here is the third one.</li>
+        </ul>
+        `,
       })
      
       setTimeout(()=>{
         console.log(html_beautify(element.innerHTML))
       },0)
-      editor.chain().insertContent(`<p> 1234</p>`).run()
+    // 
+    console.log(getDebugJSON(editor.state.doc),'调试值')
+    editor.commands.setTextSelection(3)
+    // editor.commands.sinkListItem("listItem")
+    editor.chain().focus().splitListItem('listItem').run()
+     setTimeout(()=>{
       console.log(html_beautify(element.innerHTML))
+    },10)
+    
+      
 
 }
 
